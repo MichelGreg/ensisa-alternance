557f29c698e6505fdcddd6c8ad6b31a725c3058a
Grégoire MICHEL <gregoire.michel@uha.fr>
2022-04-01 19:13:37 +0200

    Finish interface

--- src/main/java/fr/ensisa/alt/presence/Application.java
+++ src/main/java/fr/ensisa/alt/presence/Application.java
@@ -2,16 +2,19 @@ package fr.ensisa.alt.presence;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("app.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+		Scene scene = new Scene(fxmlLoader.load(), 550, 520);
 		stage.setTitle("Présence ENSISA");
+		stage.getIcons().add(new Image(Objects.requireNonNull(Application.class.getResourceAsStream("favicon.png"))));
 		stage.setScene(scene);
 		stage.show();
 	}
