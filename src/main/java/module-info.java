2955a5986f728250db62052780575d104109c716
Grégoire MICHEL <gregoire.michel@uha.fr>
2022-04-04 19:39:42 +0200

    Imports to parse ical and open excel files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module fr.ensisa.alt.presence {
 	requires javafx.fxml;
 
 	requires com.dlsc.formsfx;
+	requires org.apache.poi.ooxml;
+	requires org.mnode.ical4j.core;
 
 	opens fr.ensisa.alt.presence to javafx.fxml;
 	exports fr.ensisa.alt.presence;
